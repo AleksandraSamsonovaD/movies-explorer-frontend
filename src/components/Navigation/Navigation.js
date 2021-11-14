8a2e70b84a22a359ef8c38c831925c0c549b4be6
Aleksandra <samsonova183@gmail.com>
2021-11-14 20:41:00 +0300

    error correction 2

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,7 +7,7 @@ import profile from '../../images/profile.svg';
     <div className="navigation-wrap">
       <nav className= {`${props.loggedIn ? 'navigation_hidden' : 'navigation__noHidden'}`}>
         <NavLink className={`navigation__item ${props.loggedIn ? 'navigation__item_hidden' : ''}`} to="/sign-up">Регестрация</NavLink>
-        <NavLink className={`navigation__item ${props.loggedIn ? 'navigation__item_hidden' : ''}`} to="/sign-in"><button className="navigation__button">Войти</button></NavLink>
+        <NavLink className={`navigation__item navigation__button ${props.loggedIn ? 'navigation__item_hidden' : ''}`} to="/sign-in">Войти</NavLink>
       </nav>
       <input type ="checkbox" id="checkbox"/>
       <label htmlFor="checkbox"  className= {`${props.loggedIn ? '' : 'navigation_hidden'}`}>
@@ -19,7 +19,7 @@ import profile from '../../images/profile.svg';
         <NavLink className={`navigation__item ${props.loggedIn ? '' : 'navigation__item_hidden'}`} to="/saved-movies">Сохраненые фильмы</NavLink>
         <NavLink className={`navigation__item navigation__item_account ${props.loggedIn ? '' : 'navigation__item_hidden'}`} to="/profile"  >
           <p className="navigation__account">Аккаунт</p>
-          <div className="navigation__icon"><img alt="Аккаунт" src={profile} /></div>
+          <img className="navigation__icon" alt="Аккаунт" src={profile} />
           </NavLink>
       </nav>
     </div>
